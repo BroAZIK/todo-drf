751c46a1e9f677e04858b7e298b6f23145aa1c86
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-07 16:40:39 +0500

    auth

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Task
 
@@ -9,6 +11,9 @@ from .serializers import TaskSerializer
 
 
 class TaskView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request: Request) -> Response:
         tasks = Task.objects.all()
 
@@ -56,4 +61,3 @@ class TaskDetailView(APIView):
         task.delete()
 
         return Response({'message': 'deleted.'})
-        
