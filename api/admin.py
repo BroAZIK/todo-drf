34af178bb2ec42c943a1fe7926be6e824b760e29
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-14 16:52:48 +0500

    fndskanfkdsaf

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Task
 
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'completed', 'created']
+    list_filter = ['completed', 'created']
+    search_fields = ['title', 'description']
+    ordering = ['title', 'completed', 'created']
+    list_per_page = 10
